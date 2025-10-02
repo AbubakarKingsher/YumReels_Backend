c4e30cbc10ddf35076ee41c71e612bde5ef076c7
AbubakarKingsher <abubakarmarri007@gmail.com>
2025-10-02 21:59:59 +0500

    favion error solved

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const winston = require("winston");
 
 const PORT = process.env.PORT || 5000;
 
+app.get("/favicon.ico", (req, res) => res.status(204));
+
 app.listen(PORT, (err) => {
   if (err) return winston.error("PORT DISCONNECTED");
 });
